61a5dbceec6aa05b6036b8b47e398022514850b5
nerevarinee <rouabhi-rauf@tuta.io>
2025-02-25 17:44:06 +0100

    atlas database connection+ test (succes)

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 
 const app = express();
+
+//testing if mongodb+atlas is connected
+mongoose
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("✅ MongoDB Atlas connected successfully"))
+  .catch((err) => console.error("❌ MongoDB connection error:", err));
