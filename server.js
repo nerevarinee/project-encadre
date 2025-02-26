b560ce78a18ee1b1b4ef3ef3020aee7b019b444b
nerevarinee <rouabhi-rauf@tuta.io>
2025-02-26 20:19:26 +0100

    minor tweak

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const app = express();
 
 //testing if mongodb+atlas is connected
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB Atlas connected successfully"))
   .catch((err) => console.error("❌ MongoDB connection error:", err));
